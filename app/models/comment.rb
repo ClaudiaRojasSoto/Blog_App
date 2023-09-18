e2225cd81d860d2f65a6be28ba85e6bdfde8dc1c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-18 20:31:37 -0300

    Add the method after save and made the update comments counter as a private method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
-  # Personalized method
+  after_save :update_comments_counter
+
+  private
+
   def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
