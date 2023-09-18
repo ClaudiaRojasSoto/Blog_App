d679b2790b5bf09c83a91c6de8d83fc01a3395c7
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-18 20:29:11 -0300

    Add the after save method and made the update post counter as private method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,16 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # Personalizated methods
-  def update_post_counter
-    author.update(post_counter: author.posts.count)
-  end
+  after_save :update_post_counter
 
+  # Public Methods
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_post_counter
+    author.update(post_counter: author.posts.count)
   end
 end
