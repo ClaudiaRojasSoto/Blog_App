c0780cbe89cd557ed76cc5862190a8f2e7159964
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-18 17:53:12 -0300

    Create the model to comments

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+
+  # Personalized method
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+end
