34778b81c092667a36cdf348e3fcf244e0afa94c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-18 20:30:07 -0300

    Add the after save method and made the updtae like counter as a private method

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,10 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  # Personalizated method
+  after_save :update_likes_counter
+
+  private
+
   def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
