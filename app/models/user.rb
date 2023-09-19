6fed9e9575c1547fc3605e0a9522dd9ba413cf8c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-18 23:06:52 -0300

    Add validations for name and postsCounters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
   has_many :likes
 
-  # Personalizated method
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Personalized method
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
