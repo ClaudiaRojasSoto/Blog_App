c2bf4eba817ab25bfc386b9ee3bc3e5d00a1bd1d
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-26 13:08:34 -0300

    Add the logic to the posts methods to use pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,24 @@
+# app/controllers/posts_controller.rb
+
 class PostsController < ApplicationController
   def index
     # Catch the user which 'author_id' is the same as the parameter 'user_id' on the URL
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
 
-    # catch all the posts associates to this user
-    @posts = user.posts
+    # Catch all the posts associates to this user and paginate them
+    @posts = @user.posts.page(params[:page]).per(3)
+
+    # @posts = @user.posts.paginate(page: params[:page], per_page: 4)
 
     # Rest of the logic to show the posts on the view
   end
 
   def show
-    # found the post which 'id' is the same as the parameter 'id' on the URL
+    # Found the post which 'id' is the same as the parameter 'id' on the URL
     @post = Post.find(params[:id])
-
+    @user = User.find(params[:user_id])
+    @next_post = @user.posts.where('id > ?', @post.id).first
+    @prev_post = @user.posts.where('id < ?', @post.id).last
     # Rest of the logic to show the information of the post on the view
   end
 end
