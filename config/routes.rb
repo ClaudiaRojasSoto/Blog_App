8bec40b89fca9e5e33b368eca25f4d91a709a218
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-26 13:05:30 -0300

    Upload the routes adding the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'users#index'
   # Defines the root path route ("/")
   get '/users', to: 'users#index'
 
