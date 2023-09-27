eb71f01fb78107ea1ceeb07112988a1b42f5419c
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-09-27 14:32:14 -0300

    Add the logic on comments controller to craete a new comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,29 @@
 class CommentsController < ApplicationController
+  before_action :find_user_and_post
+
   def new
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
   def create
-    # Lógica para crear un nuevo comentario
+    @comment = @post.comments.build(comment_params)
+    @comment.author = current_user
+
+    if @comment.save
+      redirect_to user_post_path(@user, @post), notice: 'Comment was successfully created.'
+    else
+      render :new
+    end
   end
 
-  # Otras acciones, como editar, actualizar, eliminar, etc.
+  private
+
+  def find_user_and_post
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
