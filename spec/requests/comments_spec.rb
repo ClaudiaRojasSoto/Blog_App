041d8f9b8746c161e5e33d1c787ad3e4ae665eaf
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-02 15:12:22 -0300

    Add the path to the get method

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :request do
+RSpec.describe 'Comments', type: :request do
   let(:user) { create(:user) }
   let(:post) { create(:post, author: user) }
 
-  describe "GET /users/:user_id/posts/:post_id/comments" do
-    it "returns http success" do
+  describe 'GET /users/:user_id/posts/:post_id/comments' do
+    it 'returns http success' do
       get new_user_post_comment_path(user.id, post.id)
       expect(response).to have_http_status(:success)
     end
