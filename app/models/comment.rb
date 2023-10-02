f510f1a045302ffb7ce815c673f10c1aee234778
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-02 14:48:25 -0300

    Add a validation for the presence of text

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
 
+  validates :text, presence: true
+
   after_save :update_comments_counter
 
   private
