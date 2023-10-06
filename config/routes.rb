16619b16dba3eaf8001cc992f62e1cc29faa4c37
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-06 16:50:18 -0300

    Add the routes to delete the comments and post

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'users#index'
+  # resources :posts do
+  #   resources :comments
+  # end
+
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
+
+  delete '/users/:user_id/posts/:id', to: 'posts#destroy', as: 'delete_user_post'
 
   get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
   post '/users/:user_id/posts', to: 'posts#create', as: 'user_posts_create'
 
+  delete '/users/:user_id/posts/:post_id/comments/:comment_id', to: 'comments#destroy', as: 'delete_user_post_comment'
   get '/users/:user_id/posts/:post_id/comments/new', to: 'comments#new', as: 'new_user_post_comment'
   post '/users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'user_post_comments'
 
