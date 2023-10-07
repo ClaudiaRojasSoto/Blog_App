5f4ca29d7927ab5a6c9414b94e55f76e2783c09f
Dachrono <andres_zamorano_@hotmail.com>
2023-10-06 23:30:49 -0600

    Add protect from

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery except: :sign_in
 
   protected
 
