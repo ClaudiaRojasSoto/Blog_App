6527059bcd3ead4ebaad993acc486357adb078c0
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-07 02:19:38 -0300

    Add the logic to handle the post and comments on the app

---
+++ app/controllers/api/v1/post_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = current_user.posts.includes(:comments, :likes)
+    render json: @posts
+  end
+
+  def comments_for_post
+    post = Post.find(params[:post_id])
+    comments = post.comments
+    render json: comments
+  end
+
+  def create_comment
+    comment = Comment.new(post_id: params[:post_id], text: params[:comment], author_id: current_user.id)
+    if comment.save
+      render json: comment
+    else
+      render json: { error: 'Error al crear el comentario' }
+    end
+  end
+end
