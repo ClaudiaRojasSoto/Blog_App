30b8ebbf07705698d4e094c353013d84cf99a91f
Dachrono <andres_zamorano_@hotmail.com>
2023-10-06 23:34:21 -0600

    Add methods to request, register and failure request

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,6 +20,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
       resource.save
     end
   end
+
+  def respond_with(resource, _opts = {})
+    resource.persisted? ? register_success : register_failed
+  end
+
+  def register_success
+    render json: { message: 'Signed up.' }
+  end
+
+  def register_failed
+    render json: { message: 'Signed up failure.' }
+  end
   # PUT /resource
   # def update
   #   super
